3ea28a67b17bdd6efb5eb1b446d6e260527c33c5
mrpark219 <mrpark219@gmail.com>
2025-01-16 22:54:31 +0900

    ⚡️ 리포지토리 성능 개선: N + 1 문제 해결

--- src/main/kotlin/me/park/springbootkotlin/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/me/park/springbootkotlin/domain/repository/ExperienceRepository.kt
@@ -2,9 +2,14 @@ package me.park.springbootkotlin.domain.repository
 
 import me.park.springbootkotlin.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
 
+    @Query("SELECT e FROM Experience e LEFT JOIN FETCH e.details WHERE e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+
+    @Query("SELECT e FROM Experience e LEFT JOIN FETCH e.details where e.id = :id")
+    override fun findById(id: Long): Optional<Experience>
 }
--- src/main/kotlin/me/park/springbootkotlin/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/me/park/springbootkotlin/domain/repository/ProjectRepository.kt
@@ -3,9 +3,14 @@ package me.park.springbootkotlin.domain.repository
 import me.park.springbootkotlin.domain.entity.Experience
 import me.park.springbootkotlin.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
 interface ProjectRepository : JpaRepository<Project, Long> {
 
+    @Query("SELECT p FROM Project p LEFT JOIN FETCH p.skills s LEFT JOIN FETCH s.skill WHERE p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    @Query("SELECT p FROM Project p LEFT JOIN FETCH p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 }
