c573190b1136ba06cd58597b377042377c9d5bba
mrpark219 <mrpark219@gmail.com>
2025-01-16 15:10:24 +0900

    🎉 SpringBoot With Kotlin(나만의 포트폴리오 사이트 만들기) 프로젝트 시작

---
+++ src/test/kotlin/me/park/springbootkotlin/SpringbootKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.park.springbootkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringbootKotlinApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
