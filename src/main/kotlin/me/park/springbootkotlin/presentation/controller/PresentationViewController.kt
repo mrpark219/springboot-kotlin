d1fb5ec530f3cbdc1c6441d0c4d7bc34fab3126b
mrpark219 <mrpark219@gmail.com>
2025-01-17 23:02:15 +0900

    ✨ presentation - 컨트롤러 생성 및 테스트 API 추가

---
+++ src/main/kotlin/me/park/springbootkotlin/presentation/controller/PresentationViewController.kt
@@ -0,0 +1,13 @@
+package me.park.springbootkotlin.presentation.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class PresentationViewController() {
+
+    @GetMapping("/test")
+    fun test(): String {
+        return "test"
+    }
+}
