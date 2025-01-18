e3c2c2037c6dc98544542837b32e77c4c8bc2770
mrpark219 <mrpark219@gmail.com>
2025-01-18 21:26:25 +0900

    ✨ admin - AdminInterceptor 추가
    admin url로 요청 시 메뉴 정보(메뉴 목록, 페이지 정보)를 model에 담도록 변경

---
+++ src/main/kotlin/me/park/springbootkotlin/admin/interceptor/MenuDTO.kt
@@ -0,0 +1,7 @@
+package me.park.springbootkotlin.admin.interceptor
+
+data class MenuDTO(
+    val name: String,
+    val pages: List<PageDTO>
+)
+
---
+++ src/main/kotlin/me/park/springbootkotlin/admin/interceptor/PageDTO.kt
@@ -0,0 +1,6 @@
+package me.park.springbootkotlin.admin.interceptor
+
+data class PageDTO(
+    val name: String,
+    val url: String
+)
